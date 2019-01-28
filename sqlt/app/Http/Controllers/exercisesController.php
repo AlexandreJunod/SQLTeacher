58551b5ab26108761cd6a22c43ca6ac5734c0262
AlexandreJunod <36194606+AlexandreJunod@users.noreply.github.com>
2019-01-28 15:56:16 +0100

    Objectif complete. The query of the user is automaticaly corrected

--- sqlt/app/Http/Controllers/exercisesController.php
+++ sqlt/app/Http/Controllers/exercisesController.php
@@ -33,68 +33,80 @@ class exercisesController extends Controller
         foreach ($acronyms as $acronym)
         {
             $scores = score::where('people_id', '=', $acronym->id)->where('querie_id', '=', $request->question)->get(); //Select the score of the user being updated
-            if($scores == '[]') //Create a new entry
+            $prev_question = $request->question -1;
+            $prev_question = score::where('people_id', '=', $acronym->id)->where('querie_id', '=', $prev_question)->get(); //Select the previos question
+
+            if($prev_question == '[]' && $request->question > 1) //Disallow the user to answer to a question if he didn't tried the previous question
             {
-                $prev_question = $request->question -1;
-                $prev_question = score::where('people_id', '=', $acronym->id)->where('querie_id', '=', $prev_question)->get(); //Select the previos question
-
-                if($prev_question == '[]' && $request->question > 1) //Disallow the user to answer to a question if he didn't tried the previous question
-                {
-                    Session::flash('Error', 'Essayez de répondre aux questions précédents avant celle-ci');
-                }
-                else // Allow the user to send his answer
-                {
-                    $newscore = new score;
-                    $newscore->success = '0';
-                    $newscore->attempts = '0';
-                    $newscore->people_id = $acronym->id;
-                    $newscore->querie_id = $request->question;
-                    $newscore->save();
-
-                    Session::flash('Error', 'La participation à la question a été crée, veuillez valider une deuxième fois la question');
-                }
+                Session::flash('Error', 'Essayez de répondre aux questions précédents avant celle-ci');
             }
-            else //Update an existant entry
+            else // Allow the user to send his answer
             {
-                foreach($scores as $score)
+                $scores = array(0);
+                $scoreisset = 0;
+            }
+
+            foreach($scores as $score)
+            {
+                if($scoreisset == 0 || $score->success == false)
                 {
-                    if($score->success == false)
+                    $queries = querie::where('order', '=', $request->question)->get();
+
+                    foreach($queries as $querie) //Watch the query corresponding to the question answered by the user
                     {
-                        $queries = querie::where('order', '=', $request->question)->get();
 
-                        foreach($queries as $querie) //Watch the query corresponding to the question answered by the user
+                        try //try to execute a query, if it's not working, the db is probably not created
                         {
-
-                            try //try to execute a query, if it's not working, the db is probably not created
+                            DB::select($querie->statement);
+                        }
+                        catch(\Exception $e) //create DB with query given by the teacher
+                        {
+                            $exercises = exercise::where('id', '=', $querie->exercise_id)->get();
+                            foreach($exercises as $exercise)
                             {
-                                DB::select($querie->statement);
-                            }
-                            catch(\Exception $e) //create DB with query given by the teacher
-                            {
-                                $exercises = exercise::where('id', '=', $querie->exercise_id)->get();
-                                foreach($exercises as $exercise)
-                                {
-                                    $str_arr = explode(";", $exercise->statement); //Split beetwen each ";"
-                                    foreach ($str_arr as $key=>$value) { //If there is data, it will be created
-                                        if($value != '') //Sometimes it sends spaces, it prevents to return an error
-                                        DB::select($value);
-                                    }
+                                $str_arr = explode(";", $exercise->statement); //Split beetwen each ";"
+                                foreach ($str_arr as $key=>$value) { //If there is data, it will be created
+                                    if($value != '') //Sometimes it sends spaces, it prevents to return an error
+                                    DB::select($value);
                                 }
                             }
+                        }
 
-                            try //Syntax corect ?
+                        try //Query or syntax correct ?
+                        {
+                            $teacherquery = DB::select($querie->statement); //Syntax of the teacher
+                            $studentquery = DB::select($request->answer); //Syntax of the student
+
+                            if($teacherquery == $studentquery) //Compare the result of the 2 queries, if this is the same, the student has the right answer
                             {
-                                $teacherquery = DB::select($querie->statement); //Syntax of the teacher
-                                $studentquery = DB::select($request->answer); //Syntax of the student
-
-                                if($teacherquery == $studentquery) //Compare the result of the 2 queries, if this is the same, the student has the right answer
+                                if($scoreisset == 0) //Create a new entry if he hasn't a score yet
+                                {
+                                    $newanswer = new score;
+                                    $newanswer->success = '1';
+                                    $newanswer->attempts = '1';
+                                    $newanswer->people_id = $acronym->id;
+                                    $newanswer->querie_id = $request->question;
+                                    $newanswer->save();
+                                }
+                                else
                                 {
                                     $newattemp = $score->attempts +1;
-                                        score::where('people_id', '=', $acronym->id)
-                                        ->where('querie_id', '=', $request->question)
-                                        ->update([
-                                            'attempts'  =>  $newattemp,
-                                            'success'   =>  1]);
+                                    score::where('people_id', '=', $acronym->id)
+                                    ->where('querie_id', '=', $request->question)
+                                    ->update(['attempts'  =>  $newattemp,
+                                        'success'   =>  1]);
+                                }
+                            }
+                            else
+                            {
+                                if($scoreisset == 0) //Create a new entry if he hasn't a score yet
+                                {
+                                    $newanswer = new score;
+                                    $newanswer->success = '0';
+                                    $newanswer->attempts = '1';
+                                    $newanswer->people_id = $acronym->id;
+                                    $newanswer->querie_id = $request->question;
+                                    $newanswer->save();
                                 }
                                 else
                                 {
@@ -104,23 +116,35 @@ class exercisesController extends Controller
                                         ->update(['attempts' => $newattemp]);
                                 }
                             }
-                            catch(\Exception $e)
+                        }
+                        catch(\Exception $e)
+                        {
+                            if($scoreisset == 0) //Create a new entry if he hasn't a score yet
+                            {
+                                $newanswer = new score;
+                                $newanswer->success = '0';
+                                $newanswer->attempts = '1';
+                                $newanswer->people_id = $acronym->id;
+                                $newanswer->querie_id = $request->question;
+                                $newanswer->save();
+                            }
+                            else
                             {
                                 $newattemp = $score->attempts +1;
                                     score::where('people_id', '=', $acronym->id)
                                     ->where('querie_id', '=', $request->question)
                                     ->update(['attempts' => $newattemp]);
-                                Session::flash('Error', 'Syntaxe incorrecte');
+                                    Session::flash('Error', 'Requête ou syntaxe incorrecte');
                             }
                         }
                     }
-                    else //Permission denied to update the number of attemps. The user has find the right answer
-                    {
-                        Session::flash('Error', 'La réponse est déjà correcte');
-                    }
+                }
+                else //Permission denied to update the number of attemps. The user has find the right answer
+                {
+                    Session::flash('Error', 'La réponse est déjà correcte');
                 }
             }
         }
-            return redirect('exercises');
+        return redirect('exercises');
     }
 }
